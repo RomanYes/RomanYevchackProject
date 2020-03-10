7670a5a9567ed5fa01f91142da808784c0de1911
Roman Yevchack <roman25yevchack@gmail.com>
2020-03-10 16:19:27 +0200

    finish

---
+++ HW_9_3.py
@@ -0,0 +1,11 @@
+Learn more or give us feedback
+class Human:
+    pass
+class Man(Human):
+    pass
+class Woman(Human):
+    pass
+def God():
+    Adam = Man()
+    Eva = Woman()
+    return [Adam,Eva]
